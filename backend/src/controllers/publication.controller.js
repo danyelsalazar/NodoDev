fd792d2b231bcc785e459e28e29ee2cdd421916d
danyel salazar <danygasia2@gmail.com>
2026-06-30 20:35:27 -0300

    feat:validador de querys

--- backend/src/controllers/publication.controller.js
+++ backend/src/controllers/publication.controller.js
@@ -39,64 +39,55 @@ const createPublication = async (req, res) => {
   }
 };
 
-// listar las publicaciones
 const getPublications = async (req, res) => {
   try {
-    const { page = 1, limit = 10, materia } = req.query;
+    // req.query ya viene limpio, validado y con números reales gracias al middleware de validacion de parametros
+    const { page, limit, materia } = req.query;
     let filtro = {};
 
-    //si nos mandan el parametro materia
     if (materia) {
-      // Buscamos la materia usando el parametro
       const materiaEncontrada = await Subject.findOne({
         nombre: { $regex: materia, $options: "i" },
       });
 
-      // Si la materia no existe en la BD
       if (!materiaEncontrada) {
         return res.status(200).json({
           success: true,
           count: 0,
-          page: Number(page),
+          page,
           totalPages: 0,
-          message: "La materia de filtrado no existe en la BD",
+          message: "La materia de filtrado no existe in la BD",
           data: [],
         });
       }
-
-      // Si existe, asignamos su ID al filtro
       filtro.materia = materiaEncontrada._id;
     }
 
-    // Cuento la cantidad de publicaciones que cumplen con el filtro (sea el de materia o vacío para todas)
     const totalPublications = await Publication.countDocuments(filtro);
 
-    // Si no hay publicaciones, devolvemos 200 con array vacío (es un flujo correcto)
     if (totalPublications < 1) {
       return res.status(200).json({
         success: true,
         count: 0,
-        page: Number(page),
+        page,
         totalPages: 0,
         message: "No hay publicaciones disponibles para esta consulta",
         data: [],
       });
     }
 
-    // Buscamos la data paginada y ordenada
     const publications = await Publication.find(filtro)
       .sort({ createdAt: -1 })
       .populate("publicador", "nombre")
       .populate("materia", "nombre")
-      .skip((Number(page) - 1) * Number(limit)) // Aseguramos que operen como números
-      .limit(Number(limit));
+      .skip((page - 1) * limit)
+      .limit(limit);
 
-    // Mandando la data con el conteo real
     res.status(200).json({
       success: true,
       count: publications.length,
-      page: Number(page),
-      totalPages: Math.ceil(totalPublications / Number(limit)),
+      page,
+      totalPages: Math.ceil(totalPublications / limit),
       data: publications,
     });
   } catch (error) {
