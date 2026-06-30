87c3bcd2a81d6baf587ff15709ac0838dbcaa457
danyel salazar <danygasia2@gmail.com>
2026-06-29 21:58:26 -0300

    feat:organizando-admin

--- backend/src/models/SubjectModel.js
+++ backend/src/models/SubjectModel.js
@@ -1,32 +1,36 @@
 import mongoose, { Schema } from "mongoose";
 
 // cremaos nuestro esquema de materias
-const subjectSchema = new Schema({
-  nombre: {
-    type: String,
-    required: [true, "El nombre es obligatorio"],
-    unique: true,
-    trim: true,
-  },
-  carreras: {
-    type: [
-      {
-        type: String,
-        enum: {
-          values: [
+const subjectSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, "El nombre es obligatorio"],
+      unique: true,
+      trim: true,
+    },
+    carreras: {
+      type: [
+        {
+          type: String,
+          enum: [
             "Ingenieria en Sistemas",
             "Licenciatura en Informatica",
             "Tecnicatura en tecnologias web",
             "Tecnicatura en redes Informaticas",
             "Tecnicatura en analista de sistemas",
           ],
-          message: "{VALUE} no es una carrera válida",
         },
+      ],
+      required: true,
+      validate: {
+        validator: (v) => v.length > 0,
+        message: "Debe tener al menos una carrera",
       },
-    ],
-    required:[true, 'La meteria debe tener al menos una carrea a la que pertenece']
+    },
   },
-});
+  { timestamps: true },
+);
 
 // ceeo el modelo para exportar
 const Subject = mongoose.model("Subject", subjectSchema);
--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -2,59 +2,62 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import { ROLES } from "../constants/roles.js";
 
-const userSchema = new Schema({
-  nombre: {
-    type: String,
-    required: [true, "El nombre es obligatorio"],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "El correo es obligatorio"],
-    unique: true,
-    trim: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Por favor, ingresa un correo válido",
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, "La contrasena es obligatoria"],
-    validate: {
-      // valido que la contrasena cumpla con mayuscula, minuscual, numero y caracter especial
-      validator: function (valor) {
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-          valor,
-        );
+const userSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, "El nombre es obligatorio"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "El correo es obligatorio"],
+      unique: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Por favor, ingresa un correo válido",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "La contrasena es obligatoria"],
+      validate: {
+        // valido que la contrasena cumpla con mayuscula, minuscual, numero y caracter especial
+        validator: function (valor) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+            valor,
+          );
+        },
+        message:
+          "La contrasena debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&)",
       },
-      message:
-        "La contrasena debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&)",
+    },
+    publicaciones: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Publication",
+        },
+      ],
+      default: [],
+    },
+    materias: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Subject",
+        },
+      ],
+      default: [],
+    },
+    role: {
+      type: String,
+      enum: Object.values(ROLES),
     },
   },
-  publicaciones: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Publication",
-      },
-    ],
-    default: [],
-  },
-  materias: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Subject",
-      },
-    ],
-    default: [],
-  },
-  role: {
-    type: String,
-    enum: Object.values(ROLES),
-  },
-});
+  { timestamps: true },
+);
 
 // uso el metodo pre de mongo para crear un hook que va a  hashear la password antes de guardarla en la bd
 //uso function y no ()=> porque necesito usar this para hacer referencia a las propiedades que defini del schema
