55fe3c7ec34f394cde7459cc607b028191c8e734
danyel salazar <danygasia2@gmail.com>
2026-07-01 19:26:25 -0300

    feat:middleware de error global

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,16 @@
+import AppError from "../utils/AppError.js";
+
+export const errorHandler = (err, req, res, next) => {
+  // Si el error es nuestro usa su propio status, si es un error raro de Node usa 500
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  // Formato estandarizado de respuesta para toda la API
+  res.status(err.statusCode).json({
+    success: false,
+    status: err.status,
+    message: err.message,
+    // El stack trace (línea exacta del error) solo se muestra si estamos programando localmente
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+};
